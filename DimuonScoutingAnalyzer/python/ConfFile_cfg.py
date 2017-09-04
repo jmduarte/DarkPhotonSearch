d22478647b686f9575bf6e31dc315553696e634f
Javier Duarte <jduarte1@fnal.gov>
2017-09-03 20:25:37 -0500

    update to run on ParkingScoutingMonitor Run2017D

--- DimuonScoutingAnalyzer/python/ConfFile_cfg.py
+++ DimuonScoutingAnalyzer/python/ConfFile_cfg.py
@@ -16,11 +16,18 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 
 import FWCore.Utilities.FileUtils as FileUtils
-mylist = FileUtils.loadListFromFile ('infile4.txt') 
+mylist = FileUtils.loadListFromFile ('infile5.txt') 
 readFiles = cms.untracked.vstring( *mylist)
 
-process.source = cms.Source('PoolSource', fileNames = readFiles)
+myParentlist = FileUtils.loadListFromFile ('infile5_parent.txt')
+readParentFiles = cms.untracked.vstring( *myParentlist)
 
+process.source = cms.Source('PoolSource', 
+                            fileNames = readFiles,
+                            secondaryFileNames = readParentFiles)
+
+import FWCore.PythonUtilities.LumiList as LumiList
+process.source.lumisToProcess = LumiList.LumiList(filename = 'json_DCSONLY.txt').getVLuminosityBlockRange()
 
 process.TFileService = cms.Service("TFileService",
                                    fileName = cms.string('AnalysisOutput.root'),
@@ -33,11 +40,11 @@ process.TFileService = cms.Service("TFileService",
 ## )
 
 process.demo = cms.EDAnalyzer('HTScoutingAnalyzer',
-     triggerResults  = cms.InputTag("TriggerResults", "", "TEST"),
-     caloJets        = cms.InputTag("hltScoutingCaloPacker", "", "TEST"),
-     pfJets          = cms.InputTag("hltScoutingPFPacker", "", "TEST"),
+     triggerResults  = cms.InputTag("TriggerResults", "", "HLT"),
+     caloJets        = cms.InputTag("hltScoutingCaloPacker", "", "HLT"),
+     pfJets          = cms.InputTag("hltScoutingPFPacker", "", "HLT"),
      recoJets        = cms.InputTag("slimmedJets", "", "RECO"),
-     muons           = cms.InputTag("hltScoutingMuonPacker", "", "TEST"),
+     muons           = cms.InputTag("hltScoutingMuonPacker", "", "HLT"),
 )
 
 
---
+++ DimuonScoutingAnalyzer/python/multicrab_psm.py
@@ -0,0 +1,64 @@
+##########################
+# #
+# HLTPhysics Run2017A #
+# #
+##########################
+
+
+if __name__ == '__main__':
+    from CRABAPI.RawCommand import crabCommand
+
+    def submit(config):
+        res = crabCommand('submit', config = config)
+
+    from CRABClient.UserUtilities import config
+    config = config()
+
+    dataset = {
+        'ParkingScoutingMonitor-Run2017D-PromptReco-v1' : '/ParkingScoutingMonitor/Run2017D-PromptReco-v1/MINIAOD',
+        }
+    parent = {
+        'ParkingScoutingMonitor-Run2017D-PromptReco-v1' : '/ParkingScoutingMonitor/Run2017D-v1/RAW',
+        }       
+        
+    nfiles = -1 
+    filesPerJob = 10
+    masks = {
+        #'Golden' : '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/PromptReco/Cert_294927-299649_13TeV_PromptReco_Collisions17_JSON.txt'
+        'DCSonly' : '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/DCSOnly/json_DCSONLY.txt'
+        }
+    
+    for key, mask in masks.iteritems():
+        config.General.transferLogs = True
+        
+        config.JobType.maxMemoryMB = 2500
+        #config.JobType.numCores = 4
+
+        name = 'ScoutingDQMv5'
+        config.General.workArea = 'crab_'+name+'_'+key
+        config.General.transferLogs = True
+        config.JobType.pluginName = 'Analysis'
+        config.JobType.psetName = 'ConfFile_cfg.py'
+        config.Data.inputDBS = 'global'
+        config.Data.splitting = 'LumiBased'
+        config.Data.publication = True
+        config.JobType.outputFiles = ['AnalysisOutput.root']
+        #config.JobType.pyCfgParams = running_options
+        config.Site.storageSite = 'T3_US_FNALLPC' # change this depending on where you want to store things
+        config.Data.ignoreLocality = True #Required for CAF submission
+        #config.Site.whitelist = ['T3_CH_CERN_CAF'] #Required for CAF submission
+        #config.Site.ignoreGlobalBlacklist = True #Required for CAF submission
+        
+        listOfSamples = dataset.keys()
+        for sample in listOfSamples:
+            config.General.requestName = sample
+            config.Data.inputDataset = dataset[sample]
+            config.Data.secondaryInputDataset = parent[sample]        
+            config.Data.unitsPerJob = filesPerJob
+            config.Data.totalUnits = nfiles
+            config.Data.outputDatasetTag = sample
+            config.Data.lumiMask = mask
+            config.Data.outLFNDirBase = '/store/user/woodson/' + name + '_' + key # change this depending on where you want to store things (your username)
+            submit(config)
+            
+            
